4f721866caf1bc8b79c8ffa22600659b12bf6192
tanpeng <34434539+DiDrive@users.noreply.github.com>
2025-04-28 20:15:50 +0800

    新增动画

--- test/Scripts/kilzone.gd
+++ test/Scripts/kilzone.gd
@@ -4,7 +4,11 @@ extends Area2D
 
 func _on_body_entered(body:Node2D):
     print("死亡")
+    Engine.time_scale = 0.5
+    body.position.y-=20
+    body.get_node("CollisionShape2D").queue_free()
     timer.start()
 
 func _on_timer_timeout() -> void:
+    Engine.time_scale = 1
     get_tree().reload_current_scene()
--- test/Scripts/player.gd
+++ test/Scripts/player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
-
+@onready var animated_sprite = $AnimatedSprite2D
 const SPEED = 100.0
+const FAST_SPEED =150
 const JUMP_VELOCITY = -300.0
 
 
@@ -11,15 +12,23 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var direction := Input.get_axis("move_left", "move_right")
+	if Input.is_action_just_pressed("roll"):
+		velocity.x = direction * FAST_SPEED
+		animated_sprite.play("roll")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = direction * SPEED
+	if is_on_floor():
+		if direction:
+			animated_sprite.flip_h = false if direction>0 else true
+			animated_sprite.play("run")
+		else:
+			#velocity.x = move_toward(velocity.x, 0, SPEED)
+			animated_sprite.play("Idle")
+	else:
+		animated_sprite.play("jump")
+		#print("跳跃")
 
 	move_and_slide()
